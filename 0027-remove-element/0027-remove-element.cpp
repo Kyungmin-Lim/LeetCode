8e1515198aff4a155a2fcf45f7b56f7640e32f4f
Kyungmin-Lim <lkm1216@gmail.com>
2024-09-19 15:43:57 +0900

    Time: 5 ms (27.92%), Space: 10.8 MB (38.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -11,40 +11,6 @@ public:
                 dst++;
             }
         }
-        return dst;
-        
-        /*
-        // using 2 loops
-        int count = nums.size();
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i]==val)
-            {
-                nums[i] = -1;
-                count--;
-            }
-        }
-
-        for(int i=0; i<count; i++)
-        {
-            if(nums[i]==-1)
-            {
-                for(int j=i+1; j<nums.size(); j++)
-                {
-                    if(nums[j] != -1)
-                    {
-                        int tmp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tmp;
-                    }
-                }
-                
-            }
-
-        }
-
-        return count;
-        */
-        
+        return dst;              
     }
 };
