8459da25018dbfe1d92a55943dcc3e1301ae0651
Kyungmin-Lim <lkm1216@gmail.com>
2025-06-09 00:32:26 +0900

    Time: 15 ms (14.17%), Space: 14.3 MB (52.5%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,67 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+
+        Node* a = p;
+        Node* b = q;
+
+        while(a!=b)
+        {
+            if(a==NULL)
+            {
+                a = q;
+                cout << a->val << endl;
+            }
+            else
+            {
+                a = a->parent;
+                if(a==NULL)
+                {
+                    cout << "a=NULL" << endl;
+
+                }
+                else
+                {
+                    cout << a->val << endl;
+                }
+            }
+            
+
+            if(b==NULL)
+            {
+                b = p;
+                cout << b->val << endl;
+            }
+            else
+            {
+                b = b->parent;
+
+                if(b==NULL)
+                {
+                    cout << "b=NULL" << endl;
+
+                }
+                else
+                {
+                    cout << b->val << endl;
+                }
+            }
+            
+
+            
+        }
+        return a;
+        
+    }
+};
