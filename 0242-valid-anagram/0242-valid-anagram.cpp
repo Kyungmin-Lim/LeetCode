35c3c4dc7b285821a4c9bf2f3ac86967d2de217b
Kyungmin Lim <lkm1216@gmail.com>
2026-01-17 00:39:46 +0900

    Time: 110 ms (7.51%), Space: 9.5 MB (98.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,10 +5,38 @@ public:
         {
             return false;
         }
+
+        /*
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
         if(t==s)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        */
+
+        int t_num = t.size();
+        for(int i=0; i<s.size(); i++)
+        {
+            for(int j=0; j<t_num; j++)
+            {
+                if(s[i]==t[j])
+                {
+                    t.erase(j, 1);
+                    
+                    t_num--;
+                    // cout << t << endl;
+                    break;
+                }
+            }
+        }
+        
+        if(t_num==0)
         {
             return true;
         }
