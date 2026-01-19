8d9fcc64bf318ec8d8b3e243b947b81aa129e848
Kyungmin Lim <lkm1216@gmail.com>
2026-01-19 23:38:05 +0900

    Time: 0 ms (100%), Space: 40.2 MB (79.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // Time complexity: O(N)
         // Space complexity: O(N)
-
-        
+        /*
         int n = nums.size();
         vector<int> prefix(n);
         vector<int> suffix(n);
@@ -27,6 +26,31 @@ public:
         result[n-1] = prefix[n-1];
 
         return result;
-               
+        */
+
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+
+        int n = nums.size();
+        vector<int> result(n);
+        result[0] = 1;
+
+        for(int i=1; i<n; i++)
+        {
+            result[i] = result[i-1]*nums[i-1];
+            //suffix[n-i-1] = suffix[n-i]*nums[n-i];
+        }
+
+        int right_pod=1;
+        for(int i=n-2; i>=0; i--)
+        {
+            right_pod *= nums[i+1];
+            result[i] = result[i]*right_pod; 
+        }
+        
+
+        return result;
+
+
     }
 };
