c7eecbac288736939feb2ed6d81531f03c165e09
Kyungmin Lim <lkm1216@gmail.com>
2026-01-25 00:11:12 +0900

    Time: 3 ms (17.12%), Space: 8.5 MB (6.58%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -14,8 +14,33 @@ public:
 
     bool isHappy(int n) {
         int key = n;
-        int sum=0;
 
+        // unordered_map을 이용한 풀이.
+
+        unordered_map<int, bool> map;
+
+        while((n!=1)&&(map.find(n)==map.end()))
+        {
+            map[n] = true;
+            n = DigitSum(n);
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+
+
+
+        // 토끼와 거북이 풀이방법 (최적의 해)
+        // Time complexity: O(logN) - DigitSum할때 자릿수 구하는 복잡도.
+        // Space complexity: O(1)
+        /*
         int fast=n;
         int slow=n;
 
@@ -35,5 +60,6 @@ public:
             }
         }        
         return true; 
+        */
     }
 };
