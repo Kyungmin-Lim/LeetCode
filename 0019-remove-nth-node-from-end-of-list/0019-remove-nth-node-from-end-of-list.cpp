a4ccfe15b674fa022f46d66f486419bc395ac769
Kyungmin Lim <lkm1216@gmail.com>
2026-02-12 22:28:17 +0900

    Time: 0 ms (100%), Space: 14.9 MB (58.87%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,12 +10,48 @@
  */
 class Solution {
 public:
-    stack<ListNode*> s;
+    //stack<ListNode*> s;
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast;
+        ListNode* slow;
+
+        ListNode Dummy = ListNode(0);
+
+        Dummy.next = head;
+        ListNode* curr = &Dummy;
+        fast = curr;
+        slow = curr;
+
+        for(int i=0; i<n; i++)
+        {
+            fast = fast->next;            
+        }
+
+
+        while(fast->next)
+        {
+            fast = fast->next;
+            slow = slow->next;
+
+        }
+
+        slow->next = slow->next->next;
+        /*
+        if(n==1)
+        {
+            slow->next = fast->next;
+        }
+        else
+        {
+            slow->next = fast;
+        }
+        */
+
+        return Dummy.next;
         
+        /*
         // Time complexity: O(N)
         // Space complexity: O(N)
-
         ListNode Dummy(0);
         Dummy.next = head;
         ListNode* curr = &Dummy;
@@ -40,6 +76,7 @@ public:
         curr->next = dst;
 
         return Dummy.next;
+        */
 
         
     }
