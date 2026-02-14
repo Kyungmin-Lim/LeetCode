fc867e8d0c49793fc5edc44b4aa0106886c69f4a
Kyungmin Lim <lkm1216@gmail.com>
2026-02-14 17:48:49 +0900

    Time: 19 ms (5.4%), Space: 10.6 MB (40.92%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int num = s.size();
-        stack<char> word;
-        string result;
+        stack<string> buffer;
+        string word;
 
-
-        // time complexity: O(N)
-        // space cojplexity: O(N)
-
-
-        for(int i=num-1; i>=0; i--)
+        for(int i=0; i<s.size(); i++)
         {
-            if((s[i]==' ')&&(!word.empty()))
+            if(s[i]==' ')
             {
-                while(!word.empty())
+                if(word.empty())
                 {
-                    result.push_back(word.top());
-                    word.pop();                
+                    continue;
                 }
+                else
+                {
+                    buffer.push(word);
+                    word.clear();
+                }
+            }
+            else
+            {
+                word.push_back(s[i]);
+            }            
+        }
+        if(!word.empty())
+        {
+            buffer.push(word);
+            word.clear();
+        }
+        
+        string result;
+        if(buffer.empty())
+        {
+            return result;
+        }
+        int num = buffer.size();
+        for(int i=0; i<num; i++)
+        {
+            string tmp = buffer.top();
+            cout << tmp << endl;
+            for(int j=0; j<tmp.size(); j++)
+            {
+                result.push_back(tmp[j]);
+            }            
+            if(i!= num-1)
+            {
                 result.push_back(' ');
             }
-
-            if(s[i]!=' ')
-            {
-                word.push(s[i]);
-            }
-        }
-
-        while(!word.empty())
-        {
-            result.push_back(word.top());
-            word.pop();                
-        }
-        num = result.size();
-        if(result[num-1]==' ')
-        {
-            result.erase(num-1, 1);
+            buffer.pop();
         }
         return result;
-
         
     }
 };
