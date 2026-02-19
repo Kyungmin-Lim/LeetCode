0f0cc515c03ccb49828d556406180a65a7af5613
Kyungmin Lim <lkm1216@gmail.com>
2026-02-19 11:08:46 +0900

    Time: 7 ms (52.38%), Space: 14.9 MB (95.95%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,6 +17,58 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        if(!head)
+        {
+            return nullptr;
+        }
+        Node* curr = head;
+        while(curr)
+        {
+            Node* node = new Node(curr->val);
+            node->next = curr->next;
+            curr->next = node;         
+            curr = node->next;
+        }
+        curr = head;
+        while(curr)
+        {
+            if(curr->next&&curr->random)
+            {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        curr=head;
+        Node* new_head = curr->next;
+
+        Node* new_curr = new_head;
+        curr = head;
+        while(curr&&new_curr)
+        {
+            if(curr->next&&curr->next->next)
+            {
+                curr->next = curr->next->next;
+            }
+            else
+            {
+                curr->next = nullptr;
+            }
+            if(new_curr&&new_curr->next&&new_curr->next->next)
+            {
+                new_curr->next = new_curr->next->next;
+            }
+            else
+            {
+                new_curr->next = nullptr;
+            }
+            curr = curr->next;
+            new_curr = new_curr->next;
+        }
+        return new_head;
+
+
+        /*
         // Time complexity: O(N)
         // Space complexity: O(N)
         unordered_map<Node*, Node*> map;
@@ -38,5 +90,6 @@ public:
             curr = curr->next;
         }
         return map[head];        
+        */
     }
 };
