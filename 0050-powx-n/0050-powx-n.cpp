0e731f6e09a8cef10dcbc098428c3c3405256947
Kyungmin Lim <lkm1216@gmail.com>
2026-02-19 22:12:09 +0900

    Time: 0 ms (100%), Space: 8.7 MB (43.78%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(n<0)
+        {
+            x = 1/x;
+            N = -1*N;
+        }
+        return fastPow(x, N);
+    }
+    double fastPow(double x, long long n)
+    {
+        if(x==1||n==0)
+        {
+            return 1;
+        }
+        double half = fastPow(x, n/2);
+        if(n%2==0)
+        {
+            return half*half;
+        }
+        else
+        {
+            return half*half*x;
+        }
+    }
+};
