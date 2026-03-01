dc6cfe3cd0d270d25722c0432cae5cc4d5c5a75b
Kyungmin Lim <lkm1216@gmail.com>
2026-03-01 12:43:30 +0900

    Time: 35 ms (91.9%), Space: 62.4 MB (84.35%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        int start=1;
+        vector<int> path;
+        vector<vector<int>> result;
+
+        backtracking(start, n, k, path, result);
+
+        return result;        
+    }
+    void backtracking(int start, int n, int k, vector<int> &path, vector<vector<int>> &result)
+    {   
+        // Satisfied the final condition.
+        if(path.size()==k)
+        {
+            result.push_back(path);
+            return;
+        }
+
+        for(int i=start; i<=n; i++)
+        {
+            path.push_back(i);
+            backtracking(i+1, n, k, path, result);
+            path.pop_back();
+        }
+    }
+
+};
