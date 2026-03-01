86b6f29a1151036c71f0ad773f888f5699bfd170
Kyungmin Lim <lkm1216@gmail.com>
2026-03-01 14:32:22 +0900

    Time: 0 ms (100%), Space: 8.8 MB (98.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -5,34 +5,25 @@ public:
         vector<string> result;
         if(n==0)
         {
-            result.push_back("");
             return result;
         }
         
         string path;
-        unordered_map<char, string> phone;
-        phone['2'] = "abc";
-        phone['3'] = "def";
-        phone['4'] = "ghi";
-        phone['5'] = "jkl";
-        phone['6'] = "mno";
-        phone['7'] = "pqrs";
-        phone['8'] = "tuv";
-        phone['9'] = "wxyz";
+        vector<string> phone = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
         int start = 0;
         backtracking(start, n, digits, phone, path, result);
         
         return result;
     }
-    void backtracking(int start, int n, string& digits, unordered_map<char, string>& phone, string& path, vector<string>& result)
+    void backtracking(int start, int n, string& digits, vector<string>& phone, string& path, vector<string>& result)
     {
         if(path.size() == n)
         {
             result.push_back(path);
             return;
         }
-        for(char a:phone[digits[start]])
+        for(char a:phone[digits[start]-'0'])
         {
             path.push_back(a);
             backtracking(start+1, n, digits, phone, path, result);
