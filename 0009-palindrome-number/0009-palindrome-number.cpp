9bb871ad530741559240f553f71cfe2490dbb707
Kyungmin Lim <lkm1216@gmail.com>
2026-03-05 21:42:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        // Time complexity: O(N) - N is the digits of number
+        // Space complexity: O(1)
+                
         long long rev=0;
         long long num = x;
         
