e8bbb15af0dc4a93150017ff40d80102634efd9b
Kyungmin Lim <lkm1216@gmail.com>
2026-03-10 23:25:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,83 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+
+        bool rowZero = false;
+        bool colZero = false;
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        for(int i=0; i<row; i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                rowZero = true;
+            }
+        }
+
+        for(int j=0; j<col; j++)
+        {
+            if(matrix[0][j]==0)
+            {
+                colZero = true;
+            }
+        }
+
+        for(int i=1; i<row; i++)
+        {
+            for(int j=1; j<col; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<row; i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=0; j<col; j++)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int j=1; j<col; j++)
+        {
+            if(matrix[0][j]==0)
+            {
+                for(int i=0; i<row; i++)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(rowZero)
+        {
+            for(int i=0; i<row; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(colZero)
+        {
+            for(int j=0; j<col; j++)
+            {
+                matrix[0][j]=0;
+            }
+        }
+
+
+
+
+        /*
         // Time complexity: O(MxN)
         // Space complexity: O(M+N) - Big O notation is calculated as we meet the worst case. 
         // There is constant space solution!!!
@@ -40,7 +117,7 @@ public:
             col.erase(col.begin());
         }
 
-        
+        */
     
     }
 };
