e56054a5eed4d35c671e700ec6e821817050a711
Kyungmin Lim <lkm1216@gmail.com>
2026-03-14 23:26:26 +0900

    Time: 0 ms (100%), Space: 13.3 MB (44.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,66 +4,31 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
 
-        if(target<matrix[0][0] || target>matrix[row-1][col-1])
-        {
-            return false;
-        }
-
         int start = 0;
-        int end = row-1;
+        int end = row*col-1;
         int mid;
-        int row_fix=-1;
-        int col_fix;
-        
 
         while(start<=end)
         {
-            mid = (start+end)/2;
-            if(target<matrix[mid][0])
-            {
-                end = mid-1;                
-            }
-            else if(target>matrix[mid][col-1])
-            {
-                start = mid+1;                
-            }
-            else
-            {
-                row_fix = mid;
-                break;
-            }
-        }
-        if(row_fix==-1)
-        {
-            return false;
-        }
-        
+            mid = start+(end-start)/2;
+            int row_idx = mid / col;
+            int col_idx = mid % col;
 
-        start = 0;
-        end = col-1;
-        while(start<=end)
-        {
-            mid = (start+end)/2;
-            if(target<matrix[row_fix][mid])
+            if(target > matrix[row_idx][col_idx])
             {
-                end = mid-1;                
+                start = mid+1;
             }
-            else if(target>matrix[row_fix][mid])
+            else if(target < matrix[row_idx][col_idx])
             {
-                start = mid+1;                
+                end = mid-1;
             }
             else
             {
                 return true;
             }
         }
-        
-        //if(matrix[row_fix][col_fix]==target)
-        //{
-            //return true;
-        //}
-        
         return false;
-        
+
+       
     }
 };
