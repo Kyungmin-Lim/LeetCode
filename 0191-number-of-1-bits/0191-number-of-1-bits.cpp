644938a9774a743fbfceb0da38e1a24f036c64f6
Kyungmin Lim <lkm1216@gmail.com>
2026-03-15 18:00:09 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        /*
         // Time complexity: O(log2n)
         // Space complexity: O(1)
         int chk=1;
@@ -15,6 +16,17 @@ public:
             n = n >> 1;
         }
         return result;
+        */
+
+        int result=0;
+
+        while(n>0)
+        {
+            n = n & (n-1);
+            result++;
+        }
+        return result;
+
         
     }
 };
