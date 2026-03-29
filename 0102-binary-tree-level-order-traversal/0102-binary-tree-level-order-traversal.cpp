f56f7419a97a8c4c9c4080a47c19e840863f43ea
Kyungmin Lim <lkm1216@gmail.com>
2026-03-29 15:06:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,10 +11,30 @@
  */
 class Solution {
 public:
+    void DFS(TreeNode* node, int level, vector<vector<int>> &result)
+    {
+        if(!node)
+        {
+            return;
+        }
+
+        if(result.size()==level)
+        {
+            //result[level].push_back({});  // This is not correct!!!
+            result.push_back({});            
+        }
+        result[level].push_back(node->val);
+
+        DFS(node->left, level+1, result);
+        DFS(node->right, level+1, result);
+    }
+
     vector<vector<int>> levelOrder(TreeNode* root) {
+        /*
         // BFS approach.
         // Time complexity: O(N)
-        // Space complexity: O(N)
+        // Space complexity: O(W) but when full binary tree, 
+        //                   the number of last level nodes are O(N/2) = O(N).
 
         queue<TreeNode*> q;
         vector<vector<int>> result;
@@ -46,5 +66,14 @@ public:
             result.push_back(level);
         }
         return result;
+        */
+
+        // DFS approach.
+
+        vector<vector<int>> result;
+        DFS(root, 0, result);
+
+        return result;
+
     }
 };
