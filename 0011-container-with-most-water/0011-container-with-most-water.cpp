36ed8a24e1ea73f768be337f7d9670ec57a1ab0d
Kyungmin Lim <lkm1216@gmail.com>
2026-03-29 14:10:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        // Time complexity: O(n)
+        // Space complexity: O(1)
         int start = 0;
         int end = height.size()-1;
         int result=0;
