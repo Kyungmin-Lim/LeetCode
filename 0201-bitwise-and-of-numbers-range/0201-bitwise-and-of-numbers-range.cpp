a69b3f6b6142f4481b7e636250b158fd02bb6083
Kyungmin Lim <lkm1216@gmail.com>
2026-04-05 14:14:29 +0900

    Time: 2 ms (36.26%), Space: 11.1 MB (79.87%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -6,13 +6,13 @@ public:
             return left;
         }
         int result = 0;
-        int left_num = (left==0)? 1: log(left)+1;
         int count = 0;
         while(left!=0)
         {           
             if(right == left)
             {
-                result += 1<<count;
+                result = left<<count;
+                break;
             }
             left = left>>1;
             right = right>>1;
