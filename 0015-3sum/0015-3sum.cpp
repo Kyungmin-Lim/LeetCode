e3f2193aebe5e08126bc06429ad28e494bfd2d48
Kyungmin Lim <lkm1216@gmail.com>
2026-05-03 12:04:04 +0900

    Time: 59 ms (20.15%), Space: 29 MB (71.75%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        
-        // Time complexity: O(N^2)
-        // Space complexity: O(1)
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
 
         for(int i=0; i<n-2; i++)
         {
-            if((i>0)&&(nums[i]==nums[i-1]))
+            if(i>0 && nums[i]==nums[i-1])
             {
                 continue;
             }
-            if(nums[i]>0)
+            int start=i+1;
+            int end=n-1;
+            while(start<end)
             {
-                break;
+                if(-1*nums[i] == nums[start]+nums[end])
+                {
+                    result.push_back({nums[i], nums[start], nums[end]});
+                    end--;                    
+                    start++;
+
+                    while(nums[end] == nums[end+1] && end>0)
+                    {
+                        end--;
+                    }
+                    while(nums[start] == nums[start-1] && start<n-1)
+                    {
+                        start++;
+                    }
+                }
+                else if(-1*nums[i] > nums[start]+nums[end])
+                {
+                    start++;
+                    while(nums[start] == nums[start-1] && start<n-1)
+                    {
+                        start++;
+                    }
+                }
+                else //(-1*nums[i] < nums[start]+nums[end])
+                {
+                    end--;
+                    while(nums[end] == nums[end+1] && end>1)
+                    {
+                        end--;
+                    }
+                }
+
+
             }
-            int j=i+1;
-            int k=n-1;
-            while(j<k)
-            {
-                if(nums[i]+nums[j]+nums[k]==0)
-                {
-                    result.push_back({nums[i], nums[j], nums[k]});
-                    while((j<k)&&(nums[j]==nums[j+1])) j++;
-                    while((j<k)&&(nums[k]==nums[k-1])) k--;
-                    j++;
-                    k--;
-                }
-                else if(nums[i]+nums[j]+nums[k]>0)
-                {
-                    k--;
-                }
-                else
-                {
-                    j++;
-                }
-            }            
+           
         }
-
         return result;
-
         
     }
 };
