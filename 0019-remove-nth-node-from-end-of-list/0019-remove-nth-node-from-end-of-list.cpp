1fa9289b77883434bb21891356c9d0ed6830b023
Kyungmin Lim <lkm1216@gmail.com>
2026-05-04 14:29:00 +0900

    Time: 0 ms (100%), Space: 15.1 MB (24.63%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,74 +10,26 @@
  */
 class Solution {
 public:
-    //stack<ListNode*> s;
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* fast;
-        ListNode* slow;
-
         ListNode Dummy = ListNode(0);
-
         Dummy.next = head;
-        ListNode* curr = &Dummy;
-        fast = curr;
-        slow = curr;
+        ListNode *curr = &Dummy;
+        ListNode *fast = &Dummy;
 
         for(int i=0; i<n; i++)
         {
-            fast = fast->next;            
+            fast = fast->next;
         }
-
 
         while(fast->next)
         {
             fast = fast->next;
-            slow = slow->next;
-
-        }
-
-        slow->next = slow->next->next;
-        /*
-        if(n==1)
-        {
-            slow->next = fast->next;
-        }
-        else
-        {
-            slow->next = fast;
-        }
-        */
-
-        return Dummy.next;
-        
-        /*
-        // Time complexity: O(N)
-        // Space complexity: O(N)
-        ListNode Dummy(0);
-        Dummy.next = head;
-        ListNode* curr = &Dummy;
-
-        int num=0;
-
-        //s.push(Dummy);
-        while(curr)
-        {
-            num++;
-            s.push(curr);
             curr = curr->next;
         }
-        for(int i=0; i<n; i++)
-        {
-            s.pop();
-        }
 
-        curr = s.top();
-        ListNode* dst = curr->next->next;
-
-        curr->next = dst;
+        curr->next = curr->next->next;
 
         return Dummy.next;
-        */
-
         
     }
 };
