f3945dc1a161600d9888bac050504b8c22a957ff
Kyungmin Lim <lkm1216@gmail.com>
2026-05-23 15:39:47 +0900

    Time: 0 ms (100%), Space: 10.2 MB (67.69%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int buffer = 0;
+        int n = matrix[0].size();
+
+        if(n==1)
+        {
+            return;
+        }
+
+        int loop = n>>1;
+        
+
+        for(int i=0; i<loop; i++)
+        {   
+            int start = i;
+            int end = n-i-1;
+
+            for(int j=0; j<n-i*2-1; j++)
+            {
+                int buffer = matrix[start][start+j];
+                matrix[start][start+j] = matrix[end-j][start];
+                matrix[end-j][start] = matrix[end][end-j];
+                matrix[end][end-j] = matrix[start+j][end];
+                matrix[start+j][end] = buffer;
+
+            }
+        }
+    }
+};
