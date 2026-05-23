9b8583a935cb86258c7f93a5f5082a791f1bc40c
Kyungmin Lim <lkm1216@gmail.com>
2026-05-23 21:18:43 +0900

    Time: 11 ms (91.13%), Space: 24.9 MB (78.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        /*
         unordered_map<string, vector<string>> map;
 
         vector<vector<string>> result;
 
+        // Time Complexity: O(NxLlogL) - N: total number of strs, L: maximum length of strs
+        // Space Complexity: O(NxL)
         for(const string& iter:strs)
         {
             string key = iter;
@@ -16,6 +19,26 @@ public:
             result.push_back(iter.second);
         }
         return result;
+
+        */
+
+        unordered_map<string, vector<string>> map;
+        vector<vector<string>> result;
+
+        for(const string& iter:strs)
+        {
+            string key = iter;
+            sort(key.begin(), key.end());
+            map[key].push_back(iter);
+        }
+
+        for(const auto& iter:map)
+        {
+            result.push_back(iter.second);
+        }
+
+        return result;
+
 
         
     }
