f59ee771705d0ee1d67b637c234db1bf77a4380b
Kyungmin Lim <lkm1216@gmail.com>
2026-06-14 17:07:03 +0900

    Time: 0 ms (100%), Space: 97.3 MB (86.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // Time complexity: O(N)
-        // Space complexity: O(1)
-        if(prices.size()<=1)
+        int result=0;
+        int minVal=prices[0];
+        int maxVal=-1;
+
+        if(prices.size()==1)
         {
             return 0;
         }
-        
-        int minVal = prices[0];
-        int result=0;
 
-        for(int i=1;i<prices.size(); i++)
+        for(int i=1; i<prices.size(); i++)
         {
-            result = max(result, prices[i]-minVal);
-            minVal = min(minVal, prices[i]);
+            if(prices[i]<minVal)
+            {
+                minVal = prices[i];
+            }
+            if(prices[i]-minVal > result)
+            {
+                result = prices[i]-minVal;
+            }
         }
         return result;
-        
     }
 };
